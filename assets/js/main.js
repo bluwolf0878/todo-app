669ad00573137984432a40bfd39883e70423e785
blumoon0878 <andr493i@elev.techcollege.dk>
2024-08-29 10:52:01 +0200

    done and done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 // array der indeholder alle list descriptions
 let myData = []
+let curentlist = 0
 
 
-     makeDummyData()
+makeDummyData()
+makemenu()
 
 let myIcon = document.getElementById('icon')
 let myDropDown = document.getElementById('dropdownContent')
-
-
+let make = document.getElementById('make')
+let Delete = document.getElementById('Delete')
 
 myIcon.addEventListener('click', (event) => {
 
@@ -22,21 +24,21 @@ function callback(myInfo) {
     showList(myInfo)
 }
 
-function makeDummyData(){
+function makeDummyData() {
 
-   let myTodoList = {
-    name: 'liste 1',//key value pair
-    listItems: []
-}
+    let myTodoList = {
+        name: 'liste 1',//key value pair
+        listItems: []
+    }
 
-let myListItem = {
-    name: 'stå op',
-    status: true
-}
-myTodoList.listItems.push(myListItem) // adder item to itemlist
+    let myListItem = {
+        name: 'stå op',
+        status: true
+    }
+    myTodoList.listItems.push(myListItem) // adder item to itemlist
 
-myData.push(myTodoList) 
-console.log(myData);
+    myData.push(myTodoList)
+    console.log(myData);
 
 }
 
@@ -49,6 +51,7 @@ function makeList(myName) {
     }
 
     myData.push(myList)
+    curentlist = myData.length - 1
     console.table(myData)
 }
 
@@ -62,30 +65,152 @@ function makeItem(index, myName) {
     }
     myData[index].listItems.push(myListItem)
 }
+
 // modtager et index for listen, og et index for item, og fjerner dette item fra listen.
-function removeItem(listIndex, itemIndex) {
-    let myList = myData[listIndex]
+function removeItem(itemIndex) {
+    let myList = myData[curentlist]
 
     console.log(myList.listItems);
 
     myList.listItems.splice(itemIndex, 1)
 
 }
+function removeList(curentlist) {
+    myData.splice(curentlist, 1)
+    curentlist = myData.length - 1
+    showList()
+    makemenu()
+}
+// show item list and functions
 function showList(myListIndex) {
+    curentlist = myListIndex
 
     let myList = myData[myListIndex]
+    if (myList) {
 
-    let listElement = document.getElementById('container')
-    
+        let listElement = document.getElementById('container')
+        listElement.style.backgroundColor = "var(--container-list)"
+        listElement.style.margin = "0 1rem"
+        listElement.style.borderRadius = "2rem"
+        let titelelement = document.getElementById('listTitle')
+        listElement.innerHTML = ''
+        titelelement.innerHTML = `<h2>${myList.name}</h2>`
 
-    listElement.innerHTML = `<h2>${myList.name}</h2>`
+        let myHtml = ''
 
-    let myHtml = ''
+        myList.listItems.forEach((element, index) => {
+            myHtml += `<div><img onclick="deleteitemcallback(${index})" src="assets/img/Done_ring_round.svg" alt="">
+            <h3>${element.name}</h3></div>`
+        });
+        myHtml += '<div  onclick="makenewitemView()">+</div>'
 
-    myList.listItems.forEach((element, index) => {
-        myHtml += `<div><h3 onclick="itemCallBack(${index})">${element.name}</h3></div>`
+        listElement.innerHTML += myHtml
+    } else {
+        let listElement = document.getElementById('container')
+        let titelelement = document.getElementById('listTitle')
+
+        listElement.innerHTML = ''
+        titelelement.innerHTML = ''
+    }
+
+
+}
+
+function makemenu() {
+
+    let listElement = document.getElementById('dropdownContent')
+    listElement.innerHTML = ''
+
+    let myHtml = '<ul>'
+
+    myData.forEach((element, index) => {
+        myHtml += `<li onclick="itemCallBack(${index})">${element.name}</li>`
     });
+    myHtml += '</ul>'
 
     listElement.innerHTML += myHtml
 
+}
+
+function itemCallBack(index) {
+    console.log(index)
+    showList(index)
+
+}
+
+// show make new item and functions
+function makenewitemView() {
+
+    let formElement = document.getElementById('container')
+    formElement.style.backgroundColor = "var(--secondary)"
+    formElement.style.margin = "0"
+    formElement.style.borderRadius = "0"
+    formElement.innerHTML = ''
+
+
+    let myHtml = ''
+
+    myHtml += '<input type="text" placeholder="name of item" id="inputItem">'
+    myHtml += '<div class="containerbuttons">'
+    myHtml += '<button onclick="OKbuttoncallback()"> ok </button>'
+    myHtml += '<button onclick="cancelcallback()"> cancel </button>'
+    myHtml += '</div>'
+    formElement.innerHTML += myHtml
+
+}
+function OKbuttoncallback() {
+    myname = inputItem.value
+    makeItem(curentlist, myname)
+    showList(curentlist)
+}
+function cancelcallback() {
+    showList(curentlist)
+}
+
+// show make new list and functions
+make.addEventListener('click', (event) => {
+
+    makenewListView()
+
+})
+
+function makenewListView() {
+
+    let formElement = document.getElementById('container')
+    formElement.style.backgroundColor = "var(--secondary)"
+    formElement.style.margin = "0"
+    formElement.style.borderRadius = "0"
+    formElement.innerHTML = ''
+
+
+    let myHtml = ''
+
+    myHtml += '<input type="text" placeholder="name of item" id="inputItem">'
+    myHtml += '<div class="containerbuttons">'
+    myHtml += '<button onclick="okcallback()"> ok </button>'
+    myHtml += '<button onclick="Cancelcallback()"> cancel </button>'
+    myHtml += '</div>'
+    formElement.innerHTML += myHtml
+
+}
+function okcallback() {
+    myname = inputItem.value
+    makeList(myname)
+    showList(curentlist)
+    makemenu()
+}
+function Cancelcallback() {
+    showList(curentlist)
+}
+// delete event list
+Delete.addEventListener('click', (event) => {
+    console.log('delete');
+
+    removeList()
+
+})
+// delete item
+function deleteitemcallback(index) {
+    removeItem(index)
+    showList(curentlist)
 }
